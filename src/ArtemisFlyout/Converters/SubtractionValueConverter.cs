97579808b618c4310b6be8fc1255b0a08375b1b1
Cheepipe <cheerpipe@gmail.com>
2021-10-29 13:52:06 -0300

    Add Subtraction Value Converter

---
+++ src/ArtemisFlyout/Converters/SubtractionValueConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+
+namespace ArtemisFlyout.Converters
+{
+    public class SubtractionValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            object result = value;
+            double parameterValue;
+
+            if (value != null && targetType == typeof(double) &&
+                double.TryParse((string)parameter,
+                    NumberStyles.Integer, culture, out parameterValue))
+            {
+                return (double)value - (double)parameterValue;
+            }
+
+            return AvaloniaProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return AvaloniaProperty.UnsetValue;
+        }
+    }
+}

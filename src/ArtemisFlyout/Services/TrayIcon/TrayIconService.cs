153d5de71d9d43269168c4becd605e00a74bcafa
Cheepipe <cheerpipe@gmail.com>
2021-11-18 17:09:34 -0300

    Solution cleanup

--- src/ArtemisFlyout/Services/TrayIcon/TrayIconService.cs
+++ src/ArtemisFlyout/Services/TrayIcon/TrayIconService.cs
@@ -17,7 +17,7 @@ namespace ArtemisFlyout.Services
             _flyoutService.Preload();
             _trayIcon = new AvaloniaTrayIcon();
             var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
-            var icon = new WindowIcon(assets.Open(new Uri(@"resm:ArtemisFlyout.Assets.flyout.ico")));
+            var icon = new WindowIcon(assets.Open(new Uri(@"resm:ArtemisFlyout.Assets.Flyout.ico")));
             _trayIcon.Icon = icon;
             _trayIcon.Clicked += TrayIcon_Clicked;
         }
